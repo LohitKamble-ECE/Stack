a429ec594f981a6c832e09c9e05e4927f170037b
Lohit Kamble <60031554+LohitKamble-ECE@users.noreply.github.com>
2022-07-25 08:01:00 +0000

    Add function which evaluate a postfix expression

--- postfix.py
+++ postfix.py
@@ -1,10 +1,10 @@
-from typing import Dict
+from typing import Callable, Dict
 from stack import Stack
 
 
 def postfix(expression: str, precedence: Dict[str, int]):
     """Return the postfix verson of infix expression."""
-    ops = Stack()
+    ops = Stack()  # Stack of operators
     result = ''
     for symbol in expression:
         if symbol.isalnum():
@@ -29,3 +29,39 @@ def postfix(expression: str, precedence: Dict[str, int]):
     while not ops.empty():  # Pop all operators that are still there in stack.
         result += ops.pop()
     return result
+
+
+def eval_postfix(expression: str, opers: Dict[str, Callable[[int, int], int]]) -> int:
+    """
+    Evaluate the postfix expression and return the result.
+
+    Arguments:
+    expression : A postfix expression whose symbol are seperated by a space.
+    opers      : A dictionary of callable object mapped to each operator.
+
+    e.g.
+    >>> import operator
+    >>> expression = "100 200 + 2 / 5 * 7 +"
+    >>> operators = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.floordiv,
+        '^': operator.pow
+    }
+    >>> eval_postfix(expression, operators)
+    757
+    """
+    s = Stack()  # Stack of operands
+    for symbol in expression.split():
+        if symbol.isnumeric():
+            # The symbol is operand (either variable or constant). Push it into the stack.
+            s.push(int(symbol))
+        elif symbol in opers:
+            # The symbol is operator.
+            y = s.pop()  # Get second operand.
+            x = s.pop()  # Get first operand.
+            # Perform the operation base on operator.
+            result = opers[symbol](x, y)
+            s.push(result)  # Store the result back to the stack.
+    return s.pop()  # The only remaining stack item would be expression result.
